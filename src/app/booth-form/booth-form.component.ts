7f1e1f145ec97632c0b3945164cd9eb29f31085e
John Brown <jpbrown250@gmail.com>
2019-04-10 20:39:52 -0700

    Prepping to tie the booth-form component into the add-booth component.

--- src/app/booth-form/booth-form.component.ts
+++ src/app/booth-form/booth-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Booth } from '../booth';
+
 @Component({
   selector: 'app-booth-form',
   templateUrl: './booth-form.component.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoothFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // console.log(Booth.getBoothTypes());
+  }
 
   ngOnInit() {
+    console.log("booth form loaded");
   }
+  
+  // boothType: Array<"event" | "produce" | "food" | "entertainment" | "merchandise" | "other"> = 
+  //   ["event", "produce", "food", "entertainment", "merchandise", "other"];
+
+  // model = new Booth(6197506456, this.boothType[1]);
+
+  // submitted = false;
+
+  // onSubmit() { this.submitted = true; }
+
+  // // TODO: Remove this when we're done
+  // get diagnostic() { return JSON.stringify(this.model); }
 
 }
--- src/app/booth.ts
+++ src/app/booth.ts
@@ -1,11 +1,16 @@
 export class Booth {
     constructor(
-    //   public id: number,
-      public geoLocation: string, //needs class
-      public coverPhoto: string,
-      public website: string,
-      public contactNumber: number,
-      public boothType: "event" | "produce" | "food" | "entertainment" | "merchandise" | "other",
-      public otherBoothType: string,
-    ) {  }
+        // public id: number,
+        // public geoLocation: string, //needs class
+        // public coverPhoto: string,
+        // public website: string,
+        public contactNumber: number,
+        public boothType: "event" | "produce" | "food" | "entertainment" | "merchandise" | "other",
+        // TODO: only if boothType is "other"
+        // public otherBoothType: string,
+        public date: Date = new Date(),
+    ) {}
+    public getBoothTypes() {
+        return ["event", "produce", "food", "entertainment", "merchandise", "other"];
+    }
 }
